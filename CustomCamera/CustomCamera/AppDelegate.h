3a5d021d03f2d80e9c125612d4881cc6c54f453f
skj <tz_skj@126.com>
2017-10-11 15:35:15 +0800

    项目

---
+++ CustomCamera/CustomCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCamera
+//
+//  Created by 宋克军 on 2017/10/11.
+//  Copyright © 2017年 浙江艾卡资产管理有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCamera/CustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCamera
+//
+//  Created by 宋克军 on 2017/10/11.
+//  Copyright © 2017年 浙江艾卡资产管理有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
